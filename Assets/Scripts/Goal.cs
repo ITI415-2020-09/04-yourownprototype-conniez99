ae73e572c344cce14ae4a949bb951dfd8ace50b8
conniez99 <49135897+conniez99@users.noreply.github.com>
2020-10-01 23:58:14 -0400

    goal

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    static public bool goalMet = false;
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (PlayerController.count == 7)
+            {
+                goalMet = true;
+            }
+        }
+
+
+    }
+
+    void Update()
+    {
+        if (PlayerController.count == 7)
+        {
+            Material mat = GetComponent<Renderer>().material;
+            Color c = mat.color;
+            c.a = 0.5f;
+            mat.color = c;
+        }
+        if (goalMet == true)
+        {
+            Material mat = GetComponent<Renderer>().material;
+            Color c = mat.color;
+            c.a = 1;
+            mat.color = c;
+        }
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+using System.Runtime.InteropServices;
 
 public class PlayerController : MonoBehaviour
 {
+    static public int count = 0;
     public float speed = 0;
 
     private Rigidbody rb;
@@ -32,4 +34,13 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movement * speed);
     }
 
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "PickUp")
+        {
+            Destroy(collidedWith);
+            count++;
+        }
+    }
 }
