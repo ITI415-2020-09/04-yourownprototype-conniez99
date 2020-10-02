c8aa3cc80944e4100d6039731b3f0da5669d3541
conniez99 <49135897+conniez99@users.noreply.github.com>
2020-10-01 22:07:28 -0400

    s

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    void OnMouseEnter()
+    {
+        print("Player:OnMouseEnter()");
+    }
+
+    void OnMouseExit()
+    {
+        print("Player:OnMouseExit()");       
+    }
+
+}
